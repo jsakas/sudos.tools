7c6ee22929930ef8e48628085c2c540b7ca75251
Jon Sakas <jon.sakas@gmail.com>
2020-09-12 00:12:54 -0600

    Update _app.tsx

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -19,7 +19,7 @@ export default function MyApp(props: AppProps) {
   return (
     <React.Fragment>
       <Head>
-        <title>My page</title>
+        <title>Devtool</title>
         <meta name="viewport" content="minimum-scale=1, initial-scale=1, width=device-width" />
       </Head>
       <ThemeProvider theme={theme}>
